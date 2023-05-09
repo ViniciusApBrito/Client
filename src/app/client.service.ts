d1debb9cf08865c59c646934a1e1336aa0a0d845
ViniciusApBrito <vinicius.brito355@gmail.com>
2023-05-09 17:05:38 -0300

    update commit

--- src/app/client.service.ts
+++ src/app/client.service.ts
@@ -1,16 +1,31 @@
+import { FormGroup } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { ClientRectObject } from '@popperjs/core';
 import { Observable } from 'rxjs';
+import { observableToBeFn } from 'rxjs/internal/testing/TestScheduler';
 import { Client } from './client';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ClientService {
-  constructor(private http: HttpClient) {}
+  put(Client: Client) {
+    throw new Error('Method not implemented.');
+  }
+  url = 'http://localhost:3000/clients';
+  constructor(private http: HttpClient) { }
 
   getClients(): Observable<Client[]> {
-    let url = 'http://localhost:3000/clients';
-    return this.http.get<Client[]>(url);
+    return this.http.get<Client[]>(this.url);
+  }
+  save(Client: Client): Observable<Client> {
+    return this.http.post<Client>(this.url, Client);
+  }
+  update(Client: Client): Observable<Client> {
+    return this.http.put<Client>(`${this.url}/${Client.id}`, Client);
+  }
+  delete(Client: Client): Observable<void> {
+    return this.http.delete<void>(`${this.url}/${Client.id}`);
   }
 }
--- src/app/clients/clients.component.ts
+++ src/app/clients/clients.component.ts
@@ -1,6 +1,7 @@
 import { ClientService } from './../client.service';
 import { Component, OnInit } from '@angular/core';
 import { Client } from '../client';
+import { Form, FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-clients',
@@ -9,8 +10,19 @@ import { Client } from '../client';
 })
 export class ClientsComponent implements OnInit {
   Clients: Client[] = [];
+  isEditing : boolean = false;
+  FormGroupClient : FormGroup | undefined;
 
-  constructor(private ClientService: ClientService) {}
+  formGroupClient: FormGroup;
+
+  constructor(private ClientService: ClientService, private formsBuilder: FormBuilder) {
+
+    this.formGroupClient = formsBuilder.group({
+      id: [''],
+      name: [''],
+      email: ['']
+    })
+  }
 
   ngOnInit(): void {
     this.loadClients();
@@ -20,5 +32,40 @@ export class ClientsComponent implements OnInit {
     this.ClientService.getClients().subscribe({
       next: (data) => (this.Clients = data),
     });
+  }
+
+  save() {
+    if(this.isEditing){
+      this.ClientService.update(this.formGroupClient.value).subscribe({
+        next: () => {
+          this.loadClients();
+          this.formGroupClient.reset();
+          this.isEditing = false;
+        }
+      })
+    }
+    else{
+    this.ClientService.save(this.formGroupClient.value).subscribe({
+      next: data => {
+        this.Clients.push(data);
+        this.formGroupClient.reset;
+      }
+    })
+  }
+  }
+
+  edit(Client: Client) {
+    this.formGroupClient.setValue(Client)
+    this.isEditing = true
+  }
+
+  delete(Client: Client) {
+    this.ClientService.delete(Client).subscribe({
+      next: () => this.loadClients()
+    })
+  }
+  clean(){
+    this.formGroupClient.reset();
+    this.isEditing = false;
   }
 }
